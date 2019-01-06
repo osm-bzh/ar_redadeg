2bd2310f6bb02e3b1e16d00678dfed61db732256
MaelReboux <m.reboux@rennesmetropole.fr>
2019-01-06 18:00:10 +0100

    1000 m

--- load_tables_3948.sql
+++ load_tables_3948.sql
@@ -20,7 +20,7 @@ SET longueur = TRUNC( ST_Length(the_geom)::numeric / 1000 , 2) ;
 
 TRUNCATE phase_1_pk_vip_3948 ;
 INSERT INTO phase_1_pk_vip_3948
-  SELECT ogc_fid, name, description, ST_Transform(the_geom,3948) AS the_geom
+  SELECT ogc_fid, name, '', ST_Transform(the_geom,3948) AS the_geom
   FROM phase_1_pk_vip_3857 ;
 
 
@@ -33,9 +33,9 @@ INSERT INTO phase_1_trace_troncons_3948
       NULL AS km,
       NULL AS km_reel,
       NULL AS longueur,
-      ST_LineSubstring(the_geom, 940.00*n/length,
+      ST_LineSubstring(the_geom, 1000.00*n/length,
     CASE
-      WHEN 940.00*(n+1) < length THEN 940.00*(n+1)/length
+      WHEN 1000.00*(n+1) < length THEN 1000.00*(n+1)/length
       ELSE 1
     END) AS the_geom
   FROM
@@ -50,7 +50,7 @@ INSERT INTO phase_1_trace_troncons_3948
     ORDER BY secteur ASC, ordre ASC
     ) AS t
   CROSS JOIN generate_series(0,10000) AS n
-  WHERE n*940.00/length < 1
+  WHERE n*1000.00/length < 1
   ORDER BY t.secteur ASC, t.ordre ASC ;
 
 -- mise à jour des attributs
