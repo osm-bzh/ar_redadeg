c954e8a5037464b3843c1718c495854f2a550bf9
MaelReboux <m.reboux@rennesmetropole.fr>
2019-01-08 22:10:15 +0100

    en prod

--- geojson_to_postgis.sh
+++ geojson_to_postgis.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+cd /data/www/vhosts/osm-bzh-kartenn/htdocs/redadeg/scripts/
 
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # on récupère les couches geojson depuis umap
@@ -16,11 +17,11 @@ curl -sS  http://umap.openstreetmap.fr/fr/datalayer/715179/ > phase_1_pk_vip.geo
 
 # note : les coordonnées sont en 3857 maisla déclaration de la table = 4326
 
-psql -U redadeg -d redadeg -c "DROP TABLE phase_1_trace_3857 CASCADE;" 
-ogr2ogr -f "PostgreSQL" PG:"host=localhost user=redadeg password=redadeg dbname=redadeg" phase_1_trace.geojson -nln phase_1_trace_3857 -lco GEOMETRY_NAME=the_geom -explodecollections
+psql -U redadeg -d redadeg -c "DROP TABLE phase_1_trace_3857 CASCADE;"
+ogr2ogr -f "PostgreSQL" PG:"host=localhost user=redadeg password=redadeg dbname=redadeg" phase_1_trace.geojson -nln phase_1_trace_3857 -lco GEOMETRY_NAME=the_geom -explodecollections -overwrite
 
-psql -U redadeg -d redadeg -c "DROP TABLE phase_1_pk_vip_3857;" 
-ogr2ogr -f "PostgreSQL" PG:"host=localhost user=redadeg password=redadeg dbname=redadeg" phase_1_pk_vip.geojson -nln phase_1_pk_vip_3857 -lco GEOMETRY_NAME=the_geom
+psql -U redadeg -d redadeg -c "DROP TABLE phase_1_pk_vip_3857;"
+ogr2ogr -f "PostgreSQL" PG:"host=localhost user=redadeg password=redadeg dbname=redadeg" phase_1_pk_vip.geojson -nln phase_1_pk_vip_3857 -lco GEOMETRY_NAME=the_geom -explodecollections -overwrite
 
 
 # on crée les tables en 3948
@@ -28,6 +29,7 @@ psql -U redadeg -d redadeg < load_tables_3948.sql
 
 
 # et on exporte vers Geojson
+rm phase_1_pk_auto.geojson
 ogr2ogr -f "GeoJSON" phase_1_pk_auto.geojson PG:"host=localhost user=redadeg password=redadeg dbname=redadeg" phase_1_pk_auto_4326
 
 # les fichiers sont ensuite tout de suite visible dans umap
