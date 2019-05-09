22033236f06c903dffd37d9cc4969801714a559d
MaelReboux <m.reboux@rennesmetropole.fr>
2019-05-09 10:47:01 +0200

    update synchronize_routing_tables.sh

--- scripts/synchronize_routing_tables.sh
+++ scripts/synchronize_routing_tables.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-
-
 cd data
 
 # dump des tables de routage
-
-#pg_dump --file osm_roads.sql --host localhost --port 5432 --username osmbr \
-#--no-password --verbose --format=p --no-owner --section=pre-data --section=data --no-privileges --no-tablespaces --no-unlogged-table-data --no-comments \
-#--table public.osm_roads osm
 
 pg_dump --file osm_roads_pgr.sql --host 192.168.56.131 --port 5432 --username redadeg \
 --no-password --verbose --format=p --no-owner --section=pre-data --section=data --no-privileges --no-tablespaces --no-unlogged-table-data --no-comments \
@@ -24,17 +18,17 @@ pg_dump --file osm_roads_pgr_vertices_pgr.sql --host 192.168.56.131 --port 5432 
 
 
 # on zippe
+rm osm_roads_pgr.zip
 zip osm_roads_pgr.zip osm_roads_pgr.sql osm_roads_pgr_noded.sql osm_roads_pgr_vertices_pgr.sql
 
 # on envoi sur le serveur
-rsync -av --progress osm_roads_pgr.zip breizhpovh2:/data/www/vhosts/ar-redadeg_openstreetmap_bzh/htdocs/scripts/data/
+#rsync -av --progress osm_roads_pgr.zip breizhpovh2:/data/www/vhosts/ar-redadeg_openstreetmap_bzh/htdocs/scripts/data/
 
 # on envoie des commande pour maj les tables de routage
 ssh breizhpovh2 "cd /data/www/vhosts/ar-redadeg_openstreetmap_bzh/htdocs/scripts/data/ ; \
-rm osm_roads_pgr.zip osm_roads_pgr.sql osm_roads_pgr_noded.sql osm_roads_pgr_vertices_pgr.sql \
-unzip -f osm_roads_pgr.zip ; \
-psql -U redadeg -d redadeg -c 'DROP TABLE osm_roads_pgr; DROP TABLE osm_roads_pgr_noded; DROP TABLE osm_roads_pgr_vertices_pgr;'
+unzip osm_roads_pgr.zip ; \
+psql -U redadeg -d redadeg -c 'DROP TABLE IF EXISTS osm_roads_pgr; DROP TABLE IF EXISTS osm_roads_pgr_noded; DROP TABLE IF EXISTS osm_roads_pgr_vertices_pgr;' \
 psql -U redadeg -d redadeg < osm_roads_pgr.sql ; \
 psql -U redadeg -d redadeg < osm_roads_pgr_noded.sql ; \
-psql -U redadeg -d redadeg < osm_roads_pgr_vertices_pgr.sql"
-
+psql -U redadeg -d redadeg < osm_roads_pgr_vertices_pgr.sql ;\
+rm -f osm_roads_pgr.sql osm_roads_pgr_noded.sql osm_roads_pgr_vertices_pgr.sql ;"
