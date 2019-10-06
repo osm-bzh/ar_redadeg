4a0fb019ed5fbae16c5775a197f12c140663b37a
MaelReboux <m.reboux@rennesmetropole.fr>
2019-10-06 11:44:40 +0200

    Update backup_umap.sh
    données phase 2

--- scripts/backup_umap.sh
+++ scripts/backup_umap.sh
@@ -5,10 +5,21 @@ cd /data/www/vhosts/ar-redadeg_openstreetmap_bzh/htdocs/scripts/backup/
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # on récupère les couches geojson depuis umap pour sauvegarde au cas où
 
+# carte phase 1
 # le tracé manuel
 curl -sS  http://umap.openstreetmap.fr/fr/datalayer/746021/ > "jour_courant/$(date +%Y%m%d)_$(date +%H%M)_phase_1_trace.geojson"
 # PK VIP
 curl -sS  http://umap.openstreetmap.fr/fr/datalayer/715179/ > "jour_courant/$(date +%Y%m%d)_$(date +%H%M)_phase_1_pk_vip.geojson"
+
+
+#carte phase 2
+# PK secteur
+curl -sS  http://umap.openstreetmap.fr/fr/datalayer/817220/ > "jour_courant/$(date +%Y%m%d)_$(date +%H%M)_phase_2_pk_secteur.geojson"
+# PK technique / PK tecknikel
+curl -sS  http://umap.openstreetmap.fr/fr/datalayer/817221/ > "jour_courant/$(date +%Y%m%d)_$(date +%H%M)_phase_2_pk_techniques.geojson"
+# points coupe tracé
+curl -sS  http://umap.openstreetmap.fr/fr/datalayer/861810/ > "jour_courant/$(date +%Y%m%d)_$(date +%H%M)_phase_2_points_coupe_trace.geoson"
+
 
 
 # et on remonte d'un niveau ceux cd 06h 13h et 18h
@@ -18,7 +29,8 @@ curl -sS  http://umap.openstreetmap.fr/fr/datalayer/715179/ > "jour_courant/$(da
 
 for f in jour_courant/*.geojson
 do
-  if [[ $f =~ [0-9]{8}_(0600|1300|1800)_* ]]
+  #if [[ $f =~ [0-9]{8}_(0600|1300|1800)_* ]]
+  if [[ $f =~ [0-9]{8}_(0600)_* ]]
   then cp $f ./
   fi
 done
