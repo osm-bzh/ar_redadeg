51de671b49cde94be854cc7699d7e0dbabca56db
MaelReboux <m.reboux@rennesmetropole.fr>
2019-10-14 21:26:24 +0200

    maj des KM voulus

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -36,17 +36,17 @@ INSERT INTO secteur VALUES (0,'Rak-loc''han','Pré-départ',12,0);
 INSERT INTO secteur VALUES (10,'Karaez -> Rostren','Carhaix -> Rostrenen',93,818);
 INSERT INTO secteur VALUES (20,'Rostren -> Plounevez-Moedeg','Rostrenen -> Plounevez-Moedec',99,817);
 INSERT INTO secteur VALUES (30,'Plounevez-Moedeg -> Montroulez','Plounevez-Moedec -> Morlaix',230,818);
-INSERT INTO secteur VALUES (40,'Montroulez -> Ar Faou','Morlaix -> Châteauneuf-du-Faou',224,819);
+INSERT INTO secteur VALUES (40,'Montroulez -> Ar Faou','Morlaix -> Châteauneuf-du-Faou',223,819);
 INSERT INTO secteur VALUES (50,'Ar Faou -> Kemperle','Châteauneuf-du-Faou -> Quimperlé',264,818);
-INSERT INTO secteur VALUES (60,'Kemperle -> Kamorzh','Quimperlé -> Camors',272,820);
-INSERT INTO secteur VALUES (61,'Kamorzh -> Redon','Camors -> Redon',65,927);
-INSERT INTO secteur VALUES (70,'Redon -> Savenneg','Redon -> Savenay',95,862);
-INSERT INTO secteur VALUES (71,'Savenneg -> Naoned','Savenay -> Nantes',30,1616);
-INSERT INTO secteur VALUES (72,'Naoned -> Tilheg','Nantes -> Teillay',116,859);
-INSERT INTO secteur VALUES (80,'Tilheg -> Roazhon','Teillay -> Rennes',57,950);
-INSERT INTO secteur VALUES (90,'Roazhon -> Dinan','Rennes -> Dinan',228,945);
-INSERT INTO secteur VALUES (91,'Dinan -> Sant-Brieg','Dinan -> Saint-Brieuc',89,824);
-INSERT INTO secteur VALUES (100,'Sant-Brieg -> Gwengamp','Saint-Brieuc -> Gwengamp',146,821);
+INSERT INTO secteur VALUES (60,'Kemperle -> Kamorzh','Quimperlé -> Camors',212,820);
+INSERT INTO secteur VALUES (61,'Kamorzh -> Redon','Camors -> Redon',162,927);
+INSERT INTO secteur VALUES (70,'Redon -> Savenneg','Redon -> Savenay',100,862);
+INSERT INTO secteur VALUES (71,'Savenneg -> Naoned','Savenay -> Nantes',20,1616);
+INSERT INTO secteur VALUES (72,'Naoned -> Tilheg','Nantes -> Teillay',122,859);
+INSERT INTO secteur VALUES (80,'Tilheg -> Roazhon','Teillay -> Rennes',58,950);
+INSERT INTO secteur VALUES (90,'Roazhon -> Dinan','Rennes -> Dinan',215,945);
+INSERT INTO secteur VALUES (91,'Dinan -> Sant-Brieg','Dinan -> Saint-Brieuc',105,824);
+INSERT INTO secteur VALUES (100,'Sant-Brieg -> Gwengamp','Saint-Brieuc -> Gwengamp',145,821);
 INSERT INTO secteur VALUES (999,'test','test');
 
 
