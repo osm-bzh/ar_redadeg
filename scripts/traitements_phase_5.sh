82f7ae89292d1a170cf45cab0ba162903520fb37
MaelReboux <m.reboux@rennesmetropole.fr>
2019-10-22 23:54:03 +0200

    mise en place de la boucle de contrôle

--- scripts/traitements_phase_5.sh
+++ scripts/traitements_phase_5.sh
@@ -9,7 +9,8 @@
 set -e
 set -u
 
-PSQL=/usr/bin/psql
+#PSQL=/usr/bin/psql
+PSQL=psql
 DB_HOST=192.168.56.131
 DB_NAME=redadeg
 DB_USER=redadeg
@@ -38,73 +39,110 @@ echo ""
 
 
 echo "    secteur 1"
-curl -sS http://umap.openstreetmap.fr/fr/datalayer/1027042/ > data/phase_5_pk_umap_tmp.geojson
+# curl -sS http://umap.openstreetmap.fr/fr/datalayer/1027042/ > data/phase_5_pk_umap_tmp.geojson
 # chargement initial
-ogr2ogr -f "PostgreSQL" PG:"host=$DB_HOST user=$DB_USER password=$DB_PASS dbname=$DB_NAME" \
-data/phase_5_pk_umap_tmp.geojson -nln phase_5_pk_umap -lco GEOMETRY_NAME=the_geom -explodecollections -overwrite
+# ogr2ogr -f "PostgreSQL" PG:"host=$DB_HOST user=$DB_USER password=$DB_PASS dbname=$DB_NAME" \
+# data/phase_5_pk_umap_tmp.geojson -nln phase_5_pk_umap -lco GEOMETRY_NAME=the_geom -explodecollections -overwrite
 
 
 echo "    secteur 2"
-curl -sS http://umap.openstreetmap.fr/fr/datalayer/1027081/ > data/phase_5_pk_umap_tmp.geojson
+# curl -sS http://umap.openstreetmap.fr/fr/datalayer/1027081/ > data/phase_5_pk_umap_tmp.geojson
 # on rajoute à la couche
-ogr2ogr -f "PostgreSQL" PG:"host=$DB_HOST user=$DB_USER password=$DB_PASS dbname=$DB_NAME" \
-data/phase_5_pk_umap_tmp.geojson -nln phase_5_pk_umap -explodecollections -append
+# ogr2ogr -f "PostgreSQL" PG:"host=$DB_HOST user=$DB_USER password=$DB_PASS dbname=$DB_NAME" \
+# data/phase_5_pk_umap_tmp.geojson -nln phase_5_pk_umap -explodecollections -append
 
 
 echo "    secteur 3"
-curl -sS http://umap.openstreetmap.fr/fr/datalayer/1027101/ > data/phase_5_pk_umap_tmp.geojson
+# curl -sS http://umap.openstreetmap.fr/fr/datalayer/1027101/ > data/phase_5_pk_umap_tmp.geojson
 # on rajoute à la couche
-ogr2ogr -f "PostgreSQL" PG:"host=$DB_HOST user=$DB_USER password=$DB_PASS dbname=$DB_NAME" \
-data/phase_5_pk_umap_tmp.geojson -nln phase_5_pk_umap -explodecollections -append
+# ogr2ogr -f "PostgreSQL" PG:"host=$DB_HOST user=$DB_USER password=$DB_PASS dbname=$DB_NAME" \
+# data/phase_5_pk_umap_tmp.geojson -nln phase_5_pk_umap -explodecollections -append
 
 
 echo "    secteur 4"
-curl -sS http://umap.openstreetmap.fr/fr/datalayer/1027104/ > data/phase_5_pk_umap_tmp.geojson
+# curl -sS http://umap.openstreetmap.fr/fr/datalayer/1027104/ > data/phase_5_pk_umap_tmp.geojson
 # on rajoute à la couche
-ogr2ogr -f "PostgreSQL" PG:"host=$DB_HOST user=$DB_USER password=$DB_PASS dbname=$DB_NAME" \
-data/phase_5_pk_umap_tmp.geojson -nln phase_5_pk_umap -explodecollections -append
+# ogr2ogr -f "PostgreSQL" PG:"host=$DB_HOST user=$DB_USER password=$DB_PASS dbname=$DB_NAME" \
+# data/phase_5_pk_umap_tmp.geojson -nln phase_5_pk_umap -explodecollections -append
 
 
 echo "    secteur 5"
-curl -sS http://umap.openstreetmap.fr/fr/datalayer/1027107/ > data/phase_5_pk_umap_tmp.geojson
+# curl -sS http://umap.openstreetmap.fr/fr/datalayer/1027107/ > data/phase_5_pk_umap_tmp.geojson
 # on rajoute à la couche
-ogr2ogr -f "PostgreSQL" PG:"host=$DB_HOST user=$DB_USER password=$DB_PASS dbname=$DB_NAME" \
-data/phase_5_pk_umap_tmp.geojson -nln phase_5_pk_umap -explodecollections -append
+# ogr2ogr -f "PostgreSQL" PG:"host=$DB_HOST user=$DB_USER password=$DB_PASS dbname=$DB_NAME" \
+# data/phase_5_pk_umap_tmp.geojson -nln phase_5_pk_umap -explodecollections -append
 
 
 echo "    secteur 6"
-curl -sS http://umap.openstreetmap.fr/fr/datalayer/1027110/ > data/phase_5_pk_umap_tmp.geojson
+# curl -sS http://umap.openstreetmap.fr/fr/datalayer/1027110/ > data/phase_5_pk_umap_tmp.geojson
 # on rajoute à la couche
-ogr2ogr -f "PostgreSQL" PG:"host=$DB_HOST user=$DB_USER password=$DB_PASS dbname=$DB_NAME" \
-data/phase_5_pk_umap_tmp.geojson -nln phase_5_pk_umap -explodecollections -append
+# ogr2ogr -f "PostgreSQL" PG:"host=$DB_HOST user=$DB_USER password=$DB_PASS dbname=$DB_NAME" \
+# data/phase_5_pk_umap_tmp.geojson -nln phase_5_pk_umap -explodecollections -append
 
 
 echo "    secteur 7"
-curl -sS http://umap.openstreetmap.fr/fr/datalayer/1027114/ > data/phase_5_pk_umap_tmp.geojson
+# curl -sS http://umap.openstreetmap.fr/fr/datalayer/1027114/ > data/phase_5_pk_umap_tmp.geojson
 # on rajoute à la couche
-ogr2ogr -f "PostgreSQL" PG:"host=$DB_HOST user=$DB_USER password=$DB_PASS dbname=$DB_NAME" \
-data/phase_5_pk_umap_tmp.geojson -nln phase_5_pk_umap -explodecollections -append
+# ogr2ogr -f "PostgreSQL" PG:"host=$DB_HOST user=$DB_USER password=$DB_PASS dbname=$DB_NAME" \
+# data/phase_5_pk_umap_tmp.geojson -nln phase_5_pk_umap -explodecollections -append
 
 
 echo "    secteur 8"
-curl -sS http://umap.openstreetmap.fr/fr/datalayer/1027117/ > data/phase_5_pk_umap_tmp.geojson
+# curl -sS http://umap.openstreetmap.fr/fr/datalayer/1027117/ > data/phase_5_pk_umap_tmp.geojson
 # on rajoute à la couche
-ogr2ogr -f "PostgreSQL" PG:"host=$DB_HOST user=$DB_USER password=$DB_PASS dbname=$DB_NAME" \
-data/phase_5_pk_umap_tmp.geojson -nln phase_5_pk_umap -explodecollections -append
+# ogr2ogr -f "PostgreSQL" PG:"host=$DB_HOST user=$DB_USER password=$DB_PASS dbname=$DB_NAME" \
+# data/phase_5_pk_umap_tmp.geojson -nln phase_5_pk_umap -explodecollections -append
 
 
 echo "    secteur 9"
-curl -sS http://umap.openstreetmap.fr/fr/datalayer/1027120/ > data/phase_5_pk_umap_tmp.geojson
+# curl -sS http://umap.openstreetmap.fr/fr/datalayer/1027120/ > data/phase_5_pk_umap_tmp.geojson
 # on rajoute à la couche
-ogr2ogr -f "PostgreSQL" PG:"host=$DB_HOST user=$DB_USER password=$DB_PASS dbname=$DB_NAME" \
-data/phase_5_pk_umap_tmp.geojson -nln phase_5_pk_umap -explodecollections -append
+# ogr2ogr -f "PostgreSQL" PG:"host=$DB_HOST user=$DB_USER password=$DB_PASS dbname=$DB_NAME" \
+# data/phase_5_pk_umap_tmp.geojson -nln phase_5_pk_umap -explodecollections -append
 
 
 echo "    secteur 10"
-curl -sS http://umap.openstreetmap.fr/fr/datalayer/1027123/ > data/phase_5_pk_umap_tmp.geojson
+# curl -sS http://umap.openstreetmap.fr/fr/datalayer/1027123/ > data/phase_5_pk_umap_tmp.geojson
 # on rajoute à la couche
-ogr2ogr -f "PostgreSQL" PG:"host=$DB_HOST user=$DB_USER password=$DB_PASS dbname=$DB_NAME" \
-data/phase_5_pk_umap_tmp.geojson -nln phase_5_pk_umap -explodecollections -append
+# ogr2ogr -f "PostgreSQL" PG:"host=$DB_HOST user=$DB_USER password=$DB_PASS dbname=$DB_NAME" \
+# data/phase_5_pk_umap_tmp.geojson -nln phase_5_pk_umap -explodecollections -append
+
+
+
+echo ""
+echo ""
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "  Contrôle des données par secteur"
+echo ""
+
+# on veut la liste des vrais secteurs : pas des secteurs de gestion
+# on instancie donc un tableau
+
+declare -a secteursArray=()
+
+secteursArray=(`$PSQL -h $DB_HOST -U $DB_USER $DB_NAME -t -X -A -c \
+  "WITH a AS
+    (
+      SELECT substring(secteur_id::text,1, char_length(secteur_id::text)-1)::integer AS secteur_id
+      FROM phase_5_pk_ref 
+    )
+    SELECT DISTINCT(secteur_id) FROM a ORDER BY secteur_id ;"`)
+
+#echo "secteursArray = " ${secteursArray[@]}
+
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+echo ""
+echo "    1 : nombre de PK par secteur"
+echo ""
+
+for secteur in ${secteursArray[@]}
+do
+  echo "      secteur $secteur"
+
+
+  # fin de la boucle
+done
 
 
 
