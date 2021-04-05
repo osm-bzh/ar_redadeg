7f8e3e2a50bea398d568b92cb20242256c18a555
MaelREBOUX <m.reboux@rennesmetropole.fr>
2021-04-05 17:05:40 +0200

    Update hello.py

--- api/hello.py
+++ api/hello.py
@@ -11,6 +11,10 @@ app = Flask(__name__)
 def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
 
+@app.route("/about/")
+def about():
+    return "This is a simple Flask Python application"
+
 if __name__ == "__main__":
     app.debug = True
     app.run(host='0.0.0.0')
