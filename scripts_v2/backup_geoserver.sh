653674c6dfefa0ea802394ade0c841e88ce2d5bd
MaelREBOUX <m.reboux@rennesmetropole.fr>
2022-01-27 22:49:41 +0100

    Create backup_geoserver.sh

---
+++ scripts_v2/backup_geoserver.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# argument 1 passé au script = millesime redadeg
+millesime=$1
+
+zip -qr /data/projets/ar_redadeg/data/$millesime/backup/geoserver_datadir_`date +"%Y.%m.%d"`.zip /var/lib/tomcat9/webapps/geoserver/data/
