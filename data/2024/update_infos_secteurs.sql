b2f65df4a430204e0c42e54b3a8154e44514557b
MaelREBOUX <m.reboux@rennesmetropole.fr>
2023-02-21 14:26:36 +0100

    Update update_infos_secteurs.sql

--- data/2024/update_infos_secteurs.sql
+++ data/2024/update_infos_secteurs.sql
@@ -1,5 +1,9 @@
+-- table des secteurs
+-- id|nom_br|nom_fr|longueur_km|longueur|longueur_km_redadeg|node_start|node_stop|pk_start|pk_stop|
 
--- id | nom_br | nom_fr | objectif_km | km_redadeg
-TRUNCATE TABLE secteur ;
-INSERT INTO secteur VALUES (0, 'Rak-loc''han', 'Pré-départ', 0, 0);
-INSERT INTO secteur VALUES (999, 'test', 'test', NULL, NULL);
+INSERT INTO public.secteur
+(id, nom_br, nom_fr, longueur_km, longueur, longueur_km_redadeg, node_start, node_stop, pk_start, pk_stop)
+VALUES
+(999, 'test', 'test', 0, 0, 0, 0, 0, 0, 0),
+(0, 'Rak-loc''han', 'Pré-départ', 0, 0, 0, 0, 0, 0, 0)
+;
