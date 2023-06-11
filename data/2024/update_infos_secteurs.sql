6ff3314496ba0ebdf7ab9c41edad63068b27cdd5
MaelREBOUX <m.reboux@rennesmetropole.fr>
2023-06-11 23:19:56 +0200

    maj secteurs redadeg 2024

--- data/2024/update_infos_secteurs.sql
+++ data/2024/update_infos_secteurs.sql
@@ -9,13 +9,13 @@ INSERT INTO public.secteur
 VALUES
 (999, 'test', 'test', 0, 0, 0, 0, 0, 0, 0),
 (000, 'Rak-loc''han', 'Pré-départ', 0, 0, 0, 0, 0, 0, 0),
-(100, 'Beg ar Raz -> Kemperle', 'Pointe du Raz -> Quimperlé',                224, 224000, 1000,    0, 224, 0, 0),
-(200, 'Kemperle -> Ar Roc''h Bernez', 'Quimperlé -> La Roche-Bernard',       224, 224000, 1000,  225, 449, 0, 0),
-(300, 'Ar Roc''h Bernez -> Gwenvenez', 'La Roche-Bernard -> Guémené-Penfao', 224, 224000, 1000,  450, 674, 0, 0),
-(400, 'Gwenvenez -> Pleder', 'Guémené-Penfao -> Plesder',                    224, 224000, 1000,  675, 899, 0, 0),
-(500, 'Pleder -> Ploueg-ar-Mor', 'Plesder -> Plouézec',                      224, 224000, 1000,  900, 1124, 0, 0),
-(600, 'Ploueg-ar-Mor -> Plijidi', 'Plouézec -> Plésidy',                     224, 224000, 1000, 1125, 1349, 0, 0),
-(700, 'Plijidi -> Landerne', 'Plésidy -> Landerneau',                        224, 224000, 1000, 1350, 1574, 0, 0),
-(800, 'Landerne -> Kastell-Paol', 'Landerneau -> Saint-Pol-de-Léon',         224, 224000, 1000, 1575, 1799, 0, 0),
-(900, 'Kastell-Paol -> Montroulez', 'Saint-Pol-de-Léon -> Morlaix',          224, 224000, 1000, 1800, 2024, 0, 0)
+(100, 'Beg ar Raz -> Kemperle', 'Pointe du Raz -> Quimperlé',                285, 285000, 1000,    0,  285, 0, 0),
+(200, 'Kemperle -> Ar Roc''h Bernez', 'Quimperlé -> La Roche-Bernard',       200, 200000, 1000,  286,  486, 0, 0),
+(300, 'Ar Roc''h Bernez -> Gwenvenez', 'La Roche-Bernard -> Guémené-Penfao', 160, 160000, 1000,  487,  647, 0, 0),
+(400, 'Gwenvenez -> Pleder', 'Guémené-Penfao -> Plesder',                    160, 160000, 1000,  648,  808, 0, 0),
+(500, 'Pleder -> Ploueg-ar-Mor', 'Plesder -> Plouézec',                      149, 149000, 1000,  809,  958, 0, 0),
+(600, 'Ploueg-ar-Mor -> Plijidi', 'Plouézec -> Plésidy',                     270, 270000, 1000,  959, 1229, 0, 0),
+(700, 'Plijidi -> Landerne', 'Plésidy -> Landerneau',                        275, 275000, 1000, 1230, 1505, 0, 0),
+(800, 'Landerne -> Kastell-Paol', 'Landerneau -> Saint-Pol-de-Léon',         160, 160000, 1000, 1506, 1666, 0, 0),
+(900, 'Kastell-Paol -> Montroulez', 'Saint-Pol-de-Léon -> Morlaix',           32,  32000, 1000, 1667, 1699, 0, 0)
 ;
