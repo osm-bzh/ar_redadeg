d668219f5c0bbb3e100c46f1bbd7fe7c6f4cd2b6
MaelREBOUX <m.reboux@rennesmetropole.fr>
2023-09-25 08:26:08 +0200

    maj données OSM : nouveau script

--- maj_donnees_osm.md
+++ maj_donnees_osm.md
@@ -1,4 +1,4 @@
-## Mise à jour des données OpenStreetMap
+# Mise à jour des données OpenStreetMap
 
 Il est important de disposer de données OSM à jour car on s'appuie sur le filaire de voie OSM.
 
@@ -7,3 +7,45 @@ Le script [`update_db_osm.sh`](https://github.com/osm-bzh/ar_redadeg/blob/master
 Attention : 18 Go de disque consommé pour la base pour le grand ouest de la France. Plus 5 Go pour les dumps.
 
 Une mise à jour prend environ 45 minutes.
+
+
+## Prérequis
+
+### pgpass et hosts
+
+Un fichier `.pgpass` contenant les informations de connection à la base :
+
+`db.openstreetmap.local:5432:*:osmbr:****`
+
+Et une entrée dans le fichier `/etc/hosts` pour résoudre `db.openstreetmap.local`. Typiquement, si la base OSM est sur la même machine (et donc en local) :
+
+`127.0.0.1       db.openstreetmap.local`
+
+
+### Une base de données `osm`
+
+Et donc, une base de données PostGIS `osm` avec un utilisateur `osmbr`.
+
+
+```sql
+CREATE ROLE osmbr NOSUPERUSER NOCREATEDB NOCREATEROLE NOINHERIT LOGIN NOREPLICATION NOBYPASSRLS PASSWORD '****';
+
+CREATE DATABASE osm WITH OWNER = osmbr ENCODING = 'UTF8';
+```
+
+Puis dans la base : `CREATE EXTENSION postgis;`
+
+
+### Un répertoire de travail
+
+```
+sudo mkdir -p /data/dumps/
+sudo chown {user}:redadeg /data/dumps/
+```
+
+### Récupération du polygone d'extraction
+
+```bash
+wget -O /data/dumps/poly_extraction_bzh.poly https://raw.githubusercontent.com/osm-bzh/osmbr-mapstyle/master/data/poly_extraction_bzh.poly
+```
+
--- scripts_v2/update_db_osm.sh
+++ scripts_v2/update_db_osm.sh
@@ -1,46 +1,107 @@
 #! /bin/bash
 
+
+
+create_dump_breizh () {
+
+  echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+  echo "Téléchargement du dump France entière"
+  echo ""
+
+  date
+  rm -f /data/dumps/france-latest.osm.pbf
+  wget -O /data/dumps/france-latest.osm.pbf http://download.geofabrik.de/europe/france-latest.osm.pbf
+
+
+  echo ""
+  echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+  echo ""
+  echo " Decoupage dump France -> Breizh "
+  echo ""
+
+  date
+  rm -f /data/dumps/breizh.osm.pbf
+  # on le decoupe selon un polygone
+  # utiliser JOSM avec le plugin poly pour créer un fichier .poly
+  osmconvert /data/dumps/france-latest.osm.pbf -B=/data/dumps/poly_extraction_bzh.poly --complete-ways -v -o=/data/dumps/breizh.osm.pbf
+  
+}
+
+
+update_db () {
+
+  echo ""
+  echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+  echo ""
+  echo " Mise à jour de la BD OSM "
+  echo ""
+  date
+  echo ""
+
+  # on met à jour la base de données
+  # authentification dans le pgpass
+  osm2pgsql -H db.openstreetmap.local -U osmbr -d osm \
+  --hstore --slim --cache 3000 -E 3857 -v \
+  /data/dumps/breizh.osm.pbf
+
+  echo ""
+  echo " Fin de la maj de la BD "
+  date
+
+}
+
+
+
+# on teste si un dump de plus de 1 jour existe ou pas
+
+file=/data/dumps/france-latest.osm.pbf
+
+# Check if the file exists
+if [ ! -e "$file" ]; then
+    echo "Pas de dump"
+    echo ""
+    
+    create_dump_breizh
+    update_db
+
+else
+  
+  # on a un fichier donc on calcule son âge pour savoir si on passe directement à la maj ou pas
+  file_age=$(($(date +%s) - $(date -r "$file" +%s)))
+
+  # Define the threshold for one day in seconds (24 hours * 60 minutes * 60 seconds)
+  one_day=$((24 * 60 * 60))
+
+  # Compare the file age to the one-day threshold
+  if [ "$file_age" -gt "$one_day" ]; then
+      
+      #echo "Le dump est plus vieux que 1 jour"
+      create_dump_breizh
+      update_db
+
+  else
+      
+      echo "Le dump est très récent donc on ne le met pas à jour"
+
+      # Prompt the user for confirmation
+      read -p "Voulez-vous mettre à jour la base quand même (o/n)? " answer
+
+      # Check the user's response
+      if [[ "$answer" =~ ^[Oo]$ ]]; then
+          update_db
+      else
+          # on sort
+          echo "F I N"
+          exit 0
+      fi
+
+  fi
+
+fi
+
+
 echo ""
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo ""
-echo " Recuperation dump OSM France "
-echo ""
+echo "F I N"
 date
 echo ""
 
-# on efface les dumps existants
-rm -f /data/dumps/*.pbf
-
-# on récupère le dernier dump France
-wget -O /data/dumps/france-latest.osm.pbf http://download.geofabrik.de/europe/france-latest.osm.pbf
-
-echo ""
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo ""
-echo " Decoupage dump France -> Breizh "
-echo ""
-date
-echo ""
-
-# on le decoupe selon un polygone
-# utiliser JOSM avec le plugin poly pour créer un fichier .poly
-osmconvert /data/dumps/france-latest.osm.pbf -B=/data/dumps/poly_extraction_bzh.poly --complete-ways -v -o=/data/dumps/breizh.osm.pbf
-
-echo ""
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo ""
-echo " Maj de la BD OSM "
-echo ""
-date
-echo ""
-
-# on met à jour la base de données
-# authentification dans le pgpass
-osm2pgsql -H db.openstreetmap.local -U osmbr -d osm --hstore --slim --cache 3000 -E 3857 -v /data/dumps/breizh.osm.pbf 
-
-
-echo ""
-echo " Fin de la maj de la BD "
-echo ""
-date
-echo ""
