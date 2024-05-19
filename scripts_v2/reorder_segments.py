ff990d3902478d70b8eb8b400003f2d4cc0a1104
MaelREBOUX <m.reboux@rennesmetropole.fr>
2024-05-19 19:41:50 +0200

    Update reorder_segments.py

--- scripts_v2/reorder_segments.py
+++ scripts_v2/reorder_segments.py
@@ -117,7 +117,7 @@ except:
 print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 print("")
-print(" Phase 5 : création des données consolidées pour le millésime "+str(millesime))
+print(" Phase X : remise en ordre des segments")
 print("")
 
 print(" Lecture du fichier de configuration ")
@@ -146,7 +146,77 @@ print("")
 
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+# on fait un recordset avec l'ensemble des points de débuts et fin de chaque segment
+sql_get_segments = """
+SELECT
+   secteur_id, path_seq
+  --,ST_AsText(ST_StartPoint(the_geom)) AS start_point
+  ,ST_AsText(ST_ReducePrecision(ST_StartPoint(the_geom)::geometry, 1.0)) AS start_point
+  --,ST_AsText(ST_EndPoint(the_geom)) AS end_point
+  ,ST_AsText(ST_ReducePrecision(ST_EndPoint(the_geom)::geometry, 1.0)) AS end_point
+FROM public.phase_2_trace_pgr
+WHERE secteur_id = 200 AND path_seq BETWEEN 1700 AND 1710 
+--WHERE secteur_id = -1 ;
+"""
 
+db_redadeg_cursor.execute(sql_get_segments)
+segments = db_redadeg_cursor.fetchall()
+nb_total_segments = db_redadeg_cursor.rowcount
+
+print(f"  Il y a {nb_total_segments} segments à analyser")
+
+cpt_segment = 1
+
+previous_end_point = ""
+
+# on itère donc sur chaque segment
+for segment in segments:
+
+  try:
+    # print(cpt_segment)
+
+    # infos d'identification
+    secteur_id = segment[0]
+    path_seq = segment[1]
+    # vertex de début
+    start_point = segment[2]
+    # vertex de fin
+    end_point = segment[3]
+
+    # print(start_point)
+    # print(end_point)
+
+    #print(f"  {secteur_id}-{path_seq} :")
+
+    # on teste si le point de début du segment courant correspond au point de fin du segment précédent
+    # sauf si on c'est le premier segment
+    if cpt_segment == 1:
+      print(f"  {secteur_id}-{path_seq} : premier segment")
+      pass
+    else:
+      if start_point == previous_end_point:
+        print(f"  {secteur_id}-{path_seq} : ce segment est à l'endroit")
+      else:
+        print(f"  {secteur_id}-{path_seq} : ce segment est à l'envers")
+
+    # on est à la fin donc on enregistre le vertex de fin du segment courant
+    # pour pouvoir le comparer dans la prochaine itération
+    previous_end_point = end_point
+
+    cpt_segment += 1
+
+    if 
+
+  except Exception as err:
+    print("  ERREUR : " + str(err))
+    closeConnRedadegDB()
+    sys.exit()
+
+print("")
+
+del segments
+
+print("")
 
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
