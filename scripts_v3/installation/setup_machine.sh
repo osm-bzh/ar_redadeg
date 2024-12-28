7fe3dd93b495534047ee1f5e17aa3bd973358c24
Maël REBOUX <m.reboux@rennesmetropole.fr>
2024-12-28 11:31:35 +0100

    setup machine

--- scripts_v3/installation/setup_machine.sh
+++ scripts_v3/installation/setup_machine.sh
@@ -2,7 +2,10 @@
 
 # exit dès que qqch se passe mal
 # set -e
+# on continue même si qqch se passe mal
+set +e
 
+echo ""
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "  Installation des paquets de base"
 echo ""
@@ -11,6 +14,7 @@ apt update
 apt install -y lsb-release nano htop iotop multitail zip unzip git wget curl links rsync screen logrotate ca-certificates sudo
 
 
+echo ""
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "  Configuration des locales"
 echo ""
@@ -22,13 +26,15 @@ read -p "appuyez sur [Entrée] pour continuer"
 dpkg-reconfigure locales
 
 
+echo ""
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "  Installation de python"
 echo ""
 
-apt install -y python3 python-is-python3 python3-apt
+apt install -y python3 python-is-python3 python3-apt python3-venv
 
 
+echo ""
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "  Création du user redadeg"
 echo ""
@@ -39,10 +45,10 @@ adduser redadeg -u 1201 --disabled-password --quiet
 # répertoire ssh
 mkdir /home/redadeg/.ssh
 # clés autorisées
-cp ./authorized_keys /home/redadeg/.ssh/
+cp files/authorized_keys /home/redadeg/.ssh/
 # permissions
 chown -R redadeg:redadeg /home/redadeg/.ssh/
-chmod 0600 ./home/redadeg/.ssh/*
+chmod 0600 files/home/redadeg/.ssh/*
 chmod -R +x /home/redadeg/.ssh/
 
 # bashrc
@@ -50,9 +56,10 @@ cp -f bashrc_redadeg /home/redadeg/.bashrc
 chown redadeg:redadeg /home/redadeg/.bashrc
 
 # sudo
-cp -f sudoers_redadeg /etc/sudoers.d/redadeg
+cp -f files/sudoers_redadeg /etc/sudoers.d/redadeg
 
 
+echo ""
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "  Permissions sur /data/ --> user redadeg"
 echo ""
@@ -60,14 +67,16 @@ echo ""
 chown -R redadeg:redadeg /data/
 
 
+echo ""
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "  Configuration de root"
 echo ""
 
-cp ./authorized_keys /root/.ssh/
-cp -f bashrc_root /root/.bashrc
+cp files/authorized_keys /root/.ssh/
+cp -f files/bashrc_root /root/.bashrc
 
 
+echo ""
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "  Reboot"
 echo ""
