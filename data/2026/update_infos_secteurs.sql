302e0cecbfdf045047c5e4611c95568028119a7f
Maël REBOUX <m.reboux@rennesmetropole.fr>
2025-08-02 11:25:36 +0200

    Update update_infos_secteurs.sql

--- data/2026/update_infos_secteurs.sql
+++ data/2026/update_infos_secteurs.sql
@@ -8,11 +8,11 @@ INSERT INTO public.secteur
 (id, nom_br, nom_fr, longueur_km, longueur, longueur_km_redadeg, pk_start, pk_stop, node_start, node_stop)
 VALUES
 (999, 'test', 'test',                                                       0,      0,       0,    0,    0, 0, 0),
-(010, 'Lannuon -> Plijidi', 'Lannion -> Plésidy',                         220, 220000,  716.00,    0,    0, 0, 0),
-(020, 'Plijidi -> Pleiber-Krist', 'Plésidy -> Pleiber-Krist',             238, 238000,  716.00,    0,    0, 0, 0),
-(030, 'Pleiber-Krist -> Ar Faou', 'Pleiber-Krist -> Ar Faou',             285, 285000,  716.00,    0,    0, 0, 0),
-(040, 'Ar Faou -> Lomener', 'Ar Faou -> Lomener',                         300, 300000,  716.00,    0,    0, 0, 0),
-(050, 'Enez Groe', 'Enez Groe',                                            36,  36000, 1008.00 ,   0,    0, 0, 0),
-(060, 'An Oriant -> Guegon', 'An Oriant -> Guegon',                       202, 202000,  716.00,    0,    0, 0, 0),
-(070, 'Guegon -> Sant-Nikolaz-an-Hent', 'Guegon -> Sant-Nikolaz-an-Hent', 286, 286000, 1008.00,    0,    0, 0, 0),
-(080, 'Sant-Nikolaz-an-Hent -> Naoned', 'Sant-Nikolaz-an-Hent -> Naoned', 170, 170000, 1008.00,    0,    0, 0, 0);
+(100, 'Lannuon -> Plijidi', 'Lannion -> Plésidy',                         219, 219000,     716,    1,  307, 0, 0),
+(200, 'Plijidi -> Pleiber-Krist', 'Plésidy -> Pleiber-Krist',             237, 237000,     716,  308,  639, 0, 0),
+(300, 'Pleiber-Krist -> Ar Faou', 'Pleiber-Krist -> Ar Faou',             283, 283000,     716,  640, 1036, 0, 0),
+(400, 'Ar Faou -> Lomener', 'Ar Faou -> Lomener',                         299, 299000,     716, 1037, 1455, 0, 0),
+(500, 'Enez Groe', 'Enez Groe',                                            36,  36000,    1008, 1456, 1491, 0, 0),
+(600, 'An Oriant -> Guegon', 'An Oriant -> Guegon',                       201, 201000,     716, 1492, 1773, 0, 0),
+(700, 'Guegon -> Sant-Nikolaz-an-Hent', 'Guegon -> Sant-Nikolaz-an-Hent', 285, 285000,    1008, 1774, 2057, 0, 0),
+(800, 'Sant-Nikolaz-an-Hent -> Naoned', 'Sant-Nikolaz-an-Hent -> Naoned', 170, 170000,    1008, 2058, 2226, 0, 0);
