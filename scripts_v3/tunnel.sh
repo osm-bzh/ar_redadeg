4703a2bcb97f8fbeaa48be6ffeeaed83ff34c7bb
Maël REBOUX <m.reboux@rennesmetropole.fr>
2025-08-05 08:49:27 +0200

    tunnel avec autossh

--- scripts_v3/tunnel.sh
+++ scripts_v3/tunnel.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
 # crée un tunnel SSH vers le serveur PostgreSQL de ar redadeg
+# ssh -L 55432:bed110.bedniverel.bzh:5432 bed110.bedniverel.bzh
 
-ssh -L 55432:bed110.bedniverel.bzh:5432 bed110.bedniverel.bzh
+# autossh
+autossh -M 20000 -o "ServerAliveInterval 30" -t -L 55432:bed110.bedniverel.bzh:5432 bed110.bedniverel.bzh
