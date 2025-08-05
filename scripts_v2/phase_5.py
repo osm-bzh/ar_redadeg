78eb640db0bd6f310394d474f13a64a95c1b6f16
Maël REBOUX <m.reboux@rennesmetropole.fr>
2025-08-05 12:04:47 +0200

    Update phase_5.py

--- scripts_v2/phase_5.py
+++ scripts_v2/phase_5.py
@@ -547,6 +547,10 @@ WHERE phase_5_pk.pk_id = pk_recales.pk_id ;"""
   print("  fait")
 
   #
+  print("")
+  #
+
+  mkdir -p ../data/2026/export/
 
   # export GPX du tracé
   print("  Export gpx du tracé")
