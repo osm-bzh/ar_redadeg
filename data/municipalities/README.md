6d93315317c0fb60d89d359f6c6410ee32d42f4e
Maël REBOUX <m.reboux@rennesmetropole.fr>
2025-08-05 11:38:35 +0200

    new script to compute municipalities

--- data/municipalities/README.md
+++ data/municipalities/README.md
@@ -11,17 +11,8 @@ Il faut utiliser uniquement les données provenant de la base de données OSM ca
 Voir aussi : [https://github.com/osm-bzh/ar_redadeg/issues/1](https://github.com/osm-bzh/ar_redadeg/issues/1)
 
 
-## Création des données OSM
+## Exécution du script
 
-Créer les tables dans une base de données OpenStreetMap "planet" avec `create_osm_municipalities_tables.sql`.
+* se placer dans ce répertoire : `cd /data/projets/ar_redadeg/data/municipalities/`
+* lancer le script en indiquant le millésime : `./compute_municipalities.sh {millésime}`
 
-Créer les données en jouant le script `insert_into_municipalities_polygon.sql`.
-
-Exporter les données avec un backup uniquement sur les tables.
-
-
-## Import dans une base de données Ar Redadeg
-
-Créer les tables avec `create_osm_municipalities_tables.sql`.
-
-Remonter les données du backup fait à l'étape d'avant.
---
+++ data/municipalities/compute_municipalities.sh
@@ -0,0 +1,76 @@
+#! /bin/bash
+
+# exit dès que qqch se passe mal
+set -e
+# sortir si "unbound variable"
+#set -u
+
+
+if [ -z "$1" ]
+  then
+    echo "Pas de millésime en argument --> stop"
+    exit 1
+fi
+
+# lecture du fichier de configuration
+. config.sh
+
+
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "  Création de la couche des communes dans la BD OSM"
+echo ""
+
+PGPASSWORD=$DB_PASSWD $PSQL -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -f ../data/municipalities/create_osm_municipalities_tables.sql
+
+echo "  fait"
+echo ""
+
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "  Remplissage de la couche des communes dans la BD OSM"
+echo ""
+
+PGPASSWORD=$DB_PASSWD $PSQL -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -f ../data/municipalities/insert_into_municipalities_polygon.sql
+
+echo "  fait"
+echo ""
+
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "  Export de la table"
+echo ""
+
+PGPASSWORD=$osmDBPassword pg_dump --file "$rep_data/sql/osm_municipalities_polygon.sql" \
+  --host "$osmDBHost" --port "$osmDBPort" --username "$osmDBUser" --no-password \
+  --format=p --data-only --no-owner --no-privileges \
+  --table "public.osm_municipalities_polygon" "$osmDBName"
+
+echo "  fait"
+echo ""
+
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "  Vidage de la couche des communes dans la BD redadeg $1"
+echo ""
+
+PGPASSWORD=$DB_PASSWD $PSQL -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -c \
+"TRUNCATE TABLE osm_municipalities_polygon ;"
+
+PGPASSWORD=$DB_PASSWD $PSQL -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -c \
+"VACUUM FULL osm_municipalities_polygon ;"
+
+echo "  fait"
+echo ""
+
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "  Chargement de l'export dans la couche des communes dans la BD redadeg $1"
+echo ""
+
+PGPASSWORD=$DB_PASSWD $PSQL -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -f "$rep_data/sql/osm_municipalities_polygon.sql"
+
+
+echo "  fait"
+echo ""
+
+
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "  F I N "
+
+
